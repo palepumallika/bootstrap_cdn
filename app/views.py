5604b0f96ac213eba268109b125929376a01cd62
palepumallika <palepumallika@gmail.com>
2022-12-03 16:45:34 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def boot_cdn(request):
+    return render(request,'boot_cdn.html')
+def boot_cdn1(request):
+    return render(request,'boot_cdn1.html')
+def boot_cdn2(request):
+    return render(request,'boot_cdn2.html')
